a0415f675361c3fd4df5135dc471e65655724a78
Isaac <isaacsorli@gmail.com>
2026-03-16 20:29:17 +0100

    Incorporo los scripts para crear tablas de dimensiones y luego el modelo estrella.

---
+++ SCRIPTS SQL/ANOTACIONES MODELO ESTRELLA Y DIMENSIONES.md
@@ -0,0 +1,6 @@
+﻿SCRIPT para crear las tablas de dimensiones y hacer el modelo estrella
+
+1- Tenia todos los datos del excel cargados, pero ya que en clase trabajamos el hacer las tablas de dimensiones para hacer el modelo estrella y mas tarde trabajar en las vistas.
+2- Donde mas he sufrido ha sido con la tabla de nacionalidades, necesitaba hacer una tabla intermedia para poder luego trabajar sin que me diese null el buscar jugadores de una misma nacionalidad en un mismo equipo.
+3- He optimizado la información de las tablas de dimensiones para que vayan acorde a los KPI que serán orientados a las habilidad potencial de los jugadores, incluyendo un case para separar por edad, ya que mas adelante necesitaremos filtrar por edad y nos vendrá bien.
+4- Falta crear las vistas por SQL o hacer la carga en power BI, decidiré en los proximos días.
---
+++ SCRIPTS SQL/Script DIMENSIONES.sql
@@ -0,0 +1,174 @@
+create schema analitica;
+set search_path to analitica; 
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+--CREAMOS TABLAS DE DIMENSIONES--
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+
+--DIMENSION JUGADOR--
+
+create table analitica.dim_jugadores as 
+select distinct
+    jh."Futbolista_ID", 
+    jh."Nombre",
+    jh."Apellidos",
+    jh."Fecha de nacimiento",
+    jh."Edad",
+    pd."Habilidad Actual (200)",
+    pd."Habilidad Potencial (200)"
+from "PF".jugadores_hechos jh
+inner join "PF".potencial_dimensiones pd on jh."Futbolista_ID" = pd."Futbolista_ID";  -- Hago Inner porque con el join normal, los null no me dejaban hacer que la pk funcionase.
+
+-- Agrego PK, ahora si funciona porque no hay null con el JOIN.
+ALTER TABLE analitica.dim_jugadores 
+ADD PRIMARY KEY ("Futbolista_ID");
+
+select * from analitica.dim_jugadores;
+
+--DIMENSION EQUIPO--
+
+--select  * from jugadores_hechos;
+
+create  table analitica.dim_equipos as
+select distinct
+	jh."Equipo"
+from "PF".jugadores_hechos jh
+where jh."Equipo" is not null and jh."Equipo" != '';
+	
+
+alter table analitica.dim_equipos
+add column equipo_id serial primary key;
+	
+select * from analitica.dim_equipos;
+
+--DIMENSION POSICION--
+
+--select * from jugadores_hechos;
+create table analitica.dim_posiciones as 
+select distinct
+	jh."Posicion"
+from "PF".jugadores_hechos jh
+where jh."Posicion" is not null and jh."Posicion" != '';
+
+alter table analitica.dim_posiciones
+add column posicion_id serial primary key;
+
+--DIMENSION NACIONALIDAD-- --AY--
+
+create table analitica.dim_nacionalidades as
+with todas_nacionalidades as (
+    select distinct jh."Nacionalidad 1" as nacionalidad
+    from "PF".jugadores_hechos jh
+    where jh."Nacionalidad 1" is not null and jh."Nacionalidad 1" != ''
+    
+    union
+    
+    select distinct jh."Nacionalidad 2" as nacionalidad
+    from "PF".jugadores_hechos jh
+    where jh."Nacionalidad 2" is not null and jh."Nacionalidad 2" != ''
+    
+    union
+    
+    select distinct jh."Nacionalidad 3" as nacionalidad
+    from "PF".jugadores_hechos jh
+    where jh."Nacionalidad 3" is not null and jh."Nacionalidad 3" != ''
+)
+select distinct 
+    nacionalidad,
+    row_number() over (order by nacionalidad) as nacionalidad_id
+from todas_nacionalidades
+where nacionalidad is not null 
+  and nacionalidad != ''
+order by nacionalidad;
+
+-- Establecer la clave primaria
+alter table analitica.dim_nacionalidades 
+add primary key (nacionalidad_id);
+
+--SELECT * FROM analitica.dim_nacionalidades
+
+--DIMENSION NACIONALIDAD TABLA PUENTE-- --AY--
+
+create table analitica.rel_jugador_nacionalidad as
+select 
+    jh."Futbolista_ID" as jugador_id,
+    n.nacionalidad_id,
+    case 
+        when jh."Nacionalidad 1" = n.nacionalidad then 'Principal'
+        when jh."Nacionalidad 2" = n.nacionalidad then 'Secundaria'
+        when jh."Nacionalidad 3" = n.nacionalidad then 'Terciaria'
+    end as tipo_nacionalidad
+from "PF".jugadores_hechos jh
+cross join analitica.dim_nacionalidades n
+where (jh."Nacionalidad 1" = n.nacionalidad 
+    or jh."Nacionalidad 2" = n.nacionalidad 
+    or jh."Nacionalidad 3" = n.nacionalidad);
+
+-- Añadir clave primaria compuesta
+alter table analitica.rel_jugador_nacionalidad 
+add primary key (jugador_id, nacionalidad_id);
+
+-- Añadir claves foráneas
+alter table analitica.rel_jugador_nacionalidad 
+add constraint fk_jugador 
+foreign key (jugador_id) 
+references analitica.dim_jugadores ("Futbolista_ID");
+
+alter table analitica.rel_jugador_nacionalidad 
+add constraint fk_nacionalidad 
+foreign key (nacionalidad_id) 
+references analitica.dim_nacionalidades (nacionalidad_id);
+
+
+
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--HECHOS--
+--select* from jugadores_hechos;
+create table analitica.hechos_jugadores as 
+select
+    jh."Futbolista_ID" as jugador_id,
+    eq.equipo_id,
+    pos.posicion_id,
+    jh."Sueldo (Anual)" as sueldo_anual,
+    jh."Apariciones con la selección nacional" as internacionalidades,
+    jh."Goles con la selección nacional" as goles_seleccion,
+    jh."Edad",
+    case
+        when jh."Edad" < 21 then 'Sub-21'
+        when jh."Edad" between 21 and 26 then 'Jugador joven'
+        when jh."Edad" between 27 and 31 then 'Jugador en su prime'
+        when jh."Edad" >= 32 then 'Veterano'
+    end as "Experiencia jugador"
+from "PF".jugadores_hechos jh
+left join analitica.dim_equipos eq 
+on jh."Equipo" = eq."Equipo"  
+left join analitica.dim_posiciones pos 
+on jh."Posicion" = pos."Posicion";
+
+------------------------------------------------------
+--AÑADIR CLAVE PRIMARIA Y RELACIONES--
+
+delete from analitica.hechos_jugadores
+where jugador_id is null;
+alter table analitica.hechos_jugadores
+add primary key (jugador_id);
+
+------------------------------------------------------
+--Relaciones FK--
+------------------------------------------------------
+alter table analitica.hechos_jugadores
+add constraint fk_equipo
+foreign key (equipo_id) references dim_equipos(equipo_id);
+
+alter table analitica.hechos_jugadores
+add constraint fk_posicion
+foreign key (posicion_id) references dim_posiciones(posicion_id);
+
+alter table analitica.rel_jugador_nacionalidad
+add constraint fk_rel_hechos
+foreign key (jugador_id) 
+references analitica.hechos_jugadores (jugador_id);
+
